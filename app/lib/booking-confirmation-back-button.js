9fb5cf53d920cab223ba902db190cde860b7b9a8
Christopher Green <cg433n@email.arizona.edu>
2017-06-26 14:23:14 -0700

    Fixes linting error

--- app/lib/booking-confirmation-back-button.js
+++ app/lib/booking-confirmation-back-button.js
@@ -7,7 +7,7 @@ export function bookingConfirmationBackButton () {
   let parentNode = document.querySelector('body > .container')
   let backButton = document.createElement('button')
   let backButtonText = document.createTextNode('Back')
-backButton.appendChild(backButtonText)
+  backButton.appendChild(backButtonText)
   backButton.classList.add('btn', 'btn-default', 'ml3', 'mt3')
   backButton.setAttribute('onclick', 'window.history.back()')
   parentNode.insertBefore(backButton, document.getElementById('s-lc-public-nick-h1'))
