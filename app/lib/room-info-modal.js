652243c253e37c054898c009f71c893d955234a8
Christopher Green <cg433n@email.arizona.edu>
2017-06-27 11:01:39 -0700

    Changes room info button Refs #17

--- app/lib/room-info-modal.js
+++ app/lib/room-info-modal.js
@@ -38,3 +38,27 @@ export function roomInfoModal () {
 
   observer.observe(target, config)
 }
+
+// Changes styles of room info buttons
+export function roomInfoButtonStyle () {
+  let elements = document.querySelectorAll('.fc-resource-area.fc-widget-content .fc-content tr')
+
+  elements.forEach((element) => {
+    if (element.childNodes[0].classList.contains('fc-widget-content')) {
+      let infoLink = element.childNodes[0].querySelectorAll('.fc-cell-content a')[0]
+      let icon = element.childNodes[0].querySelectorAll('.fc-cell-content i.fa')[0]
+
+      // Hide fa icon
+      icon.style.display = 'none'
+
+      // Add info bubble
+      infoLink.innerHTML += `<span class="info-bubble">Info</span>`
+
+      // Hide empty cell text
+      element.childNodes[0].querySelectorAll('.fc-cell-content .fc-cell-text')[0].style.display = 'none'
+
+      // Center align cell content
+      element.childNodes[0].querySelectorAll('.fc-cell-content')[0].style.textAlign = 'center'
+    }
+  })
+}
--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ import {hideCapacity} from './lib/hide-capacity'
 import {hideBreadcrumbs} from './lib/hide-breadcrumbs'
 import {hideTinyFooter} from './lib/hide-tiny-footer'
 import {calendarClipHeight} from './lib/calendar-clip'
-import {roomInfoModal} from './lib/room-info-modal'
+import {roomInfoModal, roomInfoButtonStyle} from './lib/room-info-modal'
 import {showHeader, showFooter} from './lib/template'
 import {setMiscStyles} from './lib/set-styles'
 import {bookingConfirmationBackButton} from './lib/booking-confirmation-back-button'
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   hideTinyFooter()
   calendarClipHeight('250px')
   roomInfoModal()
+  roomInfoButtonStyle()
   setMiscStyles()
   bookingConfirmationBackButton()
   a11y()
